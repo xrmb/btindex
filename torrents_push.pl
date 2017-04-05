1a128e0c51b57d438069b37e09535a34dd64ccc1
xrmb <github@xrmb.org>
2017-04-04 23:05:23 -0400

    add: multi threading, but like every multi thread perl script it crashes

--- torrents_push.pl
+++ torrents_push.pl
@@ -1,45 +1,140 @@
 #perl
 
+use btindex;
+
 use LWP;
 use HTTP::Request::Common;
+use JSON::PP;
 
-use btindex;
+use threads;
+use Thread::Queue;
 
 use strict;
 
-my $webapi = btindex::config('webapi');
-my $ua = new LWP::UserAgent();
 
-foreach_torrent(
-  data => 'raw',
-  fromzip => 1,
-  sub
+my $webapi = btindex::config('webapi') || die 'setup webapi in config';
+
+my $qc = new Thread::Queue();
+my $qa = new Thread::Queue();
+
+$qc->limit = 1000;
+$qa->limit = 10;
+
+
+my $ta = threads->create(sub
   {
-    my ($tf, $data) = @_;
+    print("thread add start...\n");
 
-    my @tf = split('/', $tf);
+    my $ua = new LWP::UserAgent();
+    $ua->timeout(10);
 
-    printf("%s\t", $tf[-1]);
-
-    my $req = HTTP::Request::Common::GET($webapi.'/check/?'.$tf[-1]);
-    my $res = $ua->request($req);
-
-    if($res->code() == 200)
+    while(defined(my $tid = $qa->dequeue()))
     {
-      print("known\n");
-      #return;
+      my $tf = btindex::torrent_path($tid);
+      my $data = read_file($tf);
+      my @s = stat($tf);
+      my $req = HTTP::Request::Common::POST(
+          $webapi.'/add/?time='.$s[9],
+          'Content-Type' => 'application/octet-stream',
+          'Content-Length' => length($data),
+          Content => $data
+        );
+
+      my $res = $ua->request($req);
+
+      printf("add\t%s\t%s\n", $tid, $res->code());
     }
 
-    $req = HTTP::Request::Common::POST(
-        $webapi.'/add/',
-        'Content-Type' => 'application/octet-stream',
-        'Content-Length' => length($data),
-        Content => $data
-      );
-
-    $res = $ua->request($req);
-
-    printf("%s\n", $res->code());
-    exit;
+    print("thread add end...\n");
+    return;
   });
 
+
+my $tc = threads->create(sub
+  {
+    print("thread check start...\n");
+
+    my $check = sub
+    {
+      my @ids = @_;
+
+      my $ua = new LWP::UserAgent();
+      $ua->timeout(10);
+
+      my $req = HTTP::Request::Common::POST(
+          $webapi.'/mcheck/',
+          'Content-Type' => 'application/json',
+          Content => encode_json(\@ids));
+
+      my $res = $ua->request($req);
+
+      printf("check\t%s .. %s\n", substr($ids[0], 0, 18), substr($ids[-1], 0, 18));
+      if($res->code() != 200)
+      {
+        printf("check\terror %d\n", $res->code());
+        return;
+      }
+
+      $res = decode_json($res->decoded_content());
+      my $add = 0;
+      while(@$res)
+      {
+        my $id = shift(@ids);
+        my $r = shift(@$res);
+        #printf("check\t%s\t%s\n", $id, $r);
+        if($r == 404)
+        {
+          $qa->enqueue($id);
+          $add++;
+        }
+      }
+      if($add) { printf("check\t%s .. %s\t%d\n", substr($_[0], 0, 18), substr($_[-1], 0, 18), $add); }
+    };
+
+    my @ids;
+    while(defined(my $tid = $qc->dequeue()))
+    {
+      #printf("check\t%s\t%d\t%d\n", $tid, scalar(@ids), $qc->limit());
+      push(@ids, $tid);
+      if(scalar(@ids) == $qc->limit()) { $check->(@ids); @ids = (); }
+    }
+    if(@ids) { $check->(@ids); }
+    $qa->enqueue(undef);
+
+    print("thread check end...\n");
+    $ta->join();
+    return;
+  });
+
+
+my $ts = threads->create(sub
+  {
+    print("thread scan start...\n");
+    foreach_torrent(
+      start => uc($ARGV[0]),
+      sub
+      {
+        my ($tf, $data) = @_;
+
+        my @tf = split('/', $tf);
+
+        #printf("scan\t%s\t%d\n", $tf[-1], $qc->pending());
+        $qc->enqueue($tf[-1]);
+
+        return;
+      });
+    print("thread scan end...\n");
+
+    $qc->enqueue(undef);
+    $tc->join();
+    return;
+  });
+
+$ts->join();
+
+undef $qc;
+undef $qa;
+
+while(grep { $_->is_running() } threads->list()) { sleep(1); }
+
+print("all done...\n");
