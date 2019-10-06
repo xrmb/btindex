a5634e56cf645b307c7af9aa766f8f5ae3c72580
xrmb <github@xrmb.org>
2019-10-05 22:05:03 -0400

    change: download data via curl/proxy

--- db_trackers.pl
+++ db_trackers.pl
@@ -17,18 +17,20 @@ use strict;
 if($ARGV[0] eq 'create') { exit system(qq|schtasks /create /tn "$ARGV[1]\\btindex\\db_trackers" /st 00:00 /sc minute /mo 60 /tr "$^X |.Cwd::abs_path(__FILE__).qq|"|); }
 if($ARGV[0] eq 'delete') { exit system(qq|schtasks /delete /tn "$ARGV[1]\\btindex\\db_trackers"|); }
 
-my $wget = SearchPath('wgetw') || SearchPath('wget') || die 'where is wget?';
+my $wget = SearchPath('wgetw') || SearchPath('wget'); #|| die 'where is wget?';
+my $curl = SearchPath('curlw') || SearchPath('curl');
 
 my @ts = localtime();
 my $config = btindex::config();
+my $db_trackers_dl = $config->{db_trackers_dl} || Cwd::abs_path(__FILE__.'/../db_trackers.dl');
 
-#my $db = new btindex::tdb(file => __FILE__.'/../dbs/trackers', save => 10_000_000);
-#my $dbc = new btindex::tdb(file => __FILE__.'/../dbs/trackers_c', save => 10_000_000);
-#my $dbi = new btindex::tdb(file => __FILE__.'/../dbs/trackers_i', save => 10_000_000);
-#my $dbn = new btindex::tdb(file => __FILE__.'/../dbs/trackers_n', save => 10_000_000);
-#my $dbnd = new btindex::tdb(file => sprintf(__FILE__.'/../dbs/trackers_n_%04d%02d%02d', $ts[5]+1900, $ts[4]+1, $ts[3]), save => 10_000_000);
-#my $dbcd = new btindex::tdb(file => sprintf(__FILE__.'/../dbs/trackers_c_%04d%02d%02d', $ts[5]+1900, $ts[4]+1, $ts[3]), save => 10_000_000);
-#my $dbd = new btindex::tdb(file => sprintf(__FILE__.'/../dbs/trackers_%04d%02d%02d', $ts[5]+1900, $ts[4]+1, $ts[3]), save => 10_000_000);
+#my $db = new btindex::tdb(file => $config->{dbs}.'/trackers', save => 10_000_000);
+#my $dbc = new btindex::tdb(file => $config->{dbs}.'/trackers_c', save => 10_000_000);
+#my $dbi = new btindex::tdb(file => $config->{dbs}.'/trackers_i', save => 10_000_000);
+#my $dbn = new btindex::tdb(file => $config->{dbs}.'/trackers_n', save => 10_000_000);
+#my $dbnd = new btindex::tdb(file => sprintf($config->{dbs}.'/trackers_n_%04d%02d%02d', $ts[5]+1900, $ts[4]+1, $ts[3]), save => 10_000_000);
+#my $dbcd = new btindex::tdb(file => sprintf($config->{dbs}.'/trackers_c_%04d%02d%02d', $ts[5]+1900, $ts[4]+1, $ts[3]), save => 10_000_000);
+#my $dbd = new btindex::tdb(file => sprintf($config->{dbs}.'/trackers_%04d%02d%02d', $ts[5]+1900, $ts[4]+1, $ts[3]), save => 10_000_000);
 
 #$dbc->clear();
 #$dbi->clear();
@@ -37,9 +39,9 @@ my $config = btindex::config();
 open(my $log, '>>', __FILE__.'/../data/db_trackers.log');
 
 my %trackers = (
-    'http://zer0day.to/fullscrape.gz' => 'zd',
+    #'http://zer0day.to/fullscrape.gz' => 'zd',
     'http://coppersurfer.tk/full_scrape_not_a_tracker.tar.gz' => 'cs',
-    'http://scrape.leechers-paradise.org/static_scrape' => 'lp',
+    #'http://scrape.leechers-paradise.org/static_scrape' => 'lp',
     #'http://internetwarriors.net/full.tar.gz' => 'iw',
     #'http://tracker.sktorrent.net/full_scrape_not_a_tracker.tar.gz' => 'sk'
   );
@@ -48,15 +50,20 @@ TRACKER: foreach my $url (keys(%trackers))
 {
   my $short = (split(/\./, (split(/\//, $url))[2]))[-2];
 
-  my $cmd = "$wget $url -O - -T 10 -q";
+  #my $cmd = "$wget $url -O - -T 10 -q";
+  my $proxy = int(rand() * 9)+1;
+  #my $cmd = "$curl -s --connect-timeout 15 --socks5 p$proxy.xrmb.org:14480 $url --output - 2>> s:\\db_trackers.err";
+  system("$curl -s --connect-timeout 15 --socks5 p$proxy.xrmb.org:14480 $url --output $db_trackers_dl");
+  my $dl = $db_trackers_dl;
   if($ARGV[0])
   {
-    $cmd = "cat $ARGV[0]";
+    #$cmd = "cat $ARGV[0]";
+    $dl = $ARGV[0];
   }
 
-  warn $cmd;
   my $fh;
-  if(!open($fh, '-|', $cmd))
+  #if(!open($fh, '-|', $cmd))
+  if(!open($fh, '<', $dl))
   {
     if($log) { printf($log "%s\t%s\terror\n", ''.localtime(), $url); }
     next;
@@ -78,11 +85,12 @@ TRACKER: foreach my $url (keys(%trackers))
     if(read($fh, $data, 0x10000) != 0x10000) { next; }
   }
 
-  my $dbc = new btindex::tdb(file => sprintf(__FILE__.'/../dbs/trackers2_c_%04d%02d%02d_%s', $ts[5]+1900, $ts[4]+1, $ts[3], $trackers{$url}), save => 10_000_000);
-  my $dbi = new btindex::tdb(file => sprintf(__FILE__.'/../dbs/trackers2_i_%04d%02d%02d_%s', $ts[5]+1900, $ts[4]+1, $ts[3], $trackers{$url}), save => 10_000_000);
+  my $dbc = new btindex::tdb(file => sprintf($config->{dbs}.'/trackers2_c_%04d%02d%02d_%s', $ts[5]+1900, $ts[4]+1, $ts[3], $trackers{$url}), save => 10_000_000);
+  my $dbi = new btindex::tdb(file => sprintf($config->{dbs}.'/trackers2_i_%04d%02d%02d_%s', $ts[5]+1900, $ts[4]+1, $ts[3], $trackers{$url}), save => 10_000_000);
 
   my $c = 0;
   my $ac = 0;
+  my $last = '?';
   if($data =~ /:\d+:\d+\n/)
   {
     print("parsing mode: 1\n");
@@ -167,6 +175,7 @@ TRACKER: foreach my $url (keys(%trackers))
       $ic =~ m/i(\d+)e/; $ic = 1 * $1;
 
       my $tid = uc(unpack('H*', substr($data, $i-20, 20)));
+      $last = $tid;
 
       #printf("%s\t%d\t%d\n", $tid, $cc, $ic);
 
@@ -176,7 +185,7 @@ TRACKER: foreach my $url (keys(%trackers))
       if(!$id && $ic) { $id = $dbi->sid($tid, add => \$added); }
       if($added)
       {
-        if($ac % 100 == 0)
+        if($ac % 1000 == 0)
         {
           printf("%s\t%08x\t%d\t%d\t%.2f%%\n", $tid, $id, $ac, $c, $ac/$c*100);
         }
@@ -211,6 +220,10 @@ TRACKER: foreach my $url (keys(%trackers))
 
   Whatsup->record(app => 'db_trackers', $short => $c);
   if($log) { printf($log "%s\t%s\t%d\t%d\n", ''.localtime(), $url, $c, $ac); }
+
+  open(my $ll, '>>', __FILE__.'/../data/db_trackers.log');
+  if($ll) { printf($ll "%s\t%s\t%d\t%d\t%s\n", ''.localtime(), $url, $c, $ac, $last); }
+  close($ll);
 
   last if($ARGV[0]);
 }
